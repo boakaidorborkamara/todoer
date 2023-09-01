83b46e47735c6070595cb022171db830ef8fc004
boakaidkamara <boakaidkamara@gmail.com>
2023-09-01 22:34:55 +0000

    count number of tasks

--- src/App.js
+++ src/App.js
@@ -7,14 +7,13 @@ import FilterButton from "./components/FilterButton";
 
 function App(props) {
   const [tasks, setTask] = useState(props.tasks);
-  console.log(tasks);
+  const [task_counter, setTaskCounter] = useState(tasks.length);
+  console.log(task_counter);
 
   function addTask(task_name) {
-    console.log(task_name);
-    console.log("TASKS", tasks);
     let new_task = { id: uuidv4(), name: task_name.name, completed: false };
     setTask([...tasks, new_task]);
-    console.log(tasks);
+    setTaskCounter(task_counter + 1);
   }
 
   const taskList = tasks.map((task) => (
@@ -31,7 +30,7 @@ function App(props) {
       <h1>ToDoer</h1>
       <Form addTask={addTask} />
       <FilterButton />
-      <h2 id="list-heading">3 tasks remaining</h2>
+      <h2 id="list-heading">{task_counter} tasks remaining</h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
